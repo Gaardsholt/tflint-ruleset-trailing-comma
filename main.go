731f988b26d513b7519fed2834a4198e69d57e2f
Lasse Gaardsholt <lasse.gaardsholt@bestseller.com>
2026-02-18 23:43:13 +0100

    fix: set version dynamically in goreleaser configuration and main.go
    Signed-off-by: Lasse Gaardsholt <lasse.gaardsholt@bestseller.com>

--- main.go
+++ main.go
@@ -6,11 +6,15 @@ import (
 	"github.com/terraform-linters/tflint-plugin-sdk/tflint"
 )
 
+var (
+	version = "0.2.0"
+)
+
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		RuleSet: &tflint.BuiltinRuleSet{
 			Name:    "trailing-comma",
-			Version: "0.2.0",
+			Version: version,
 			Rules: []tflint.Rule{
 				rules.NewTerraformListsTrailingCommaRule(),
 				rules.NewTerraformMapTrailingCommaRule(),
